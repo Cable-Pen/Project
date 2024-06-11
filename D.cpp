ffbf1b1e12d2b336de9b9a8513b541d5c8bece47
Cable-Pen <162283541+Cable-Pen@users.noreply.github.com>
2024-06-11 09:02:47 +0330

    Update D.cpp

--- D.cpp
+++ D.cpp
@@ -5,19 +5,25 @@ class vector{
         int size;
         int* array;
     public:
+        vector(){
+            size=0;
+            array= new int[size];
+        }
         void resize(int newsize){
             size=newsize;
             int *newarray=new int[size];
             for(int i=0;i<size;i++){
                 cin>>newarray[i];
                 }
+             delete[] array;
             array= newarray;
-            delete[] newarray;
+         
         }
         void showarray(){
-            cout<<size;
             for(int i=0;i<size;i++)
-            cout<<array[i];}
+                cout<<array[i];
+            cout<<"size= "<<size<<endl;
+            }
         void swap(int value,int iindex){
             if (iindex>=size)
                 cout<<"invalid index";
@@ -27,16 +33,29 @@ class vector{
         void popback(){
              size=size-1;
             int *newarray=new int[size];
-            for(int i=0;i<size;i++){
+            for(int i=0;i<size;i++)
+            {
             newarray[i]=array[i];
-            delete[] newarray;
-            array= newarray;
             }
+            delete[] array;
+            array= newarray;
         }
-        
+        void pushback(){
+             size=size+1;
+            int *newarray=new int[size];
+            for(int i=0;i<size-1;i++)
+            {
+            newarray[i]=array[i];
+            }
+            cin>>newarray[size-1];
+                delete[] array;
+            array= newarray;
+
+            
+        }
 };
 
 using namespace std;
 int main() {
     
-}
+}                                                                                                       
